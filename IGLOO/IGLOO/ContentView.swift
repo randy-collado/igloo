11f4a303bd099e9b2c2ae344a331f01f14f52906
randy-collado <58373826+randy-collado@users.noreply.github.com>
2019-11-30 17:36:00 -0500

    Add files via upload

---
+++ IGLOO/IGLOO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IGLOO
+//
+//  Created by Randy  on 11/30/19.
+//  Copyright © 2019 Randy . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is a test of IGLOO app")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
